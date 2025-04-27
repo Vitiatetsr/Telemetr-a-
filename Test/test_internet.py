0842cd475effd6111663480e72869c333b200fdf
edRan21 <165865586+edRan21@users.noreply.github.com>
2025-04-26 22:25:54 -0600

    Delete Test directory

--- Test/test_internet.py
+++
@@ -1,8 +0,0 @@
-from Core.Network.InternetManager import InternetManager
-
-if __name__=="__main__":
-    print("Conexión inicial:", InternetManager.is_connected())
-    if InternetManager.wait_for_connection(retries=3, delay=2):
-        print("Internet disponible tras esperar.")
-    else:
-        print("No se recuperó la conexión despúes de varios intentos.")
