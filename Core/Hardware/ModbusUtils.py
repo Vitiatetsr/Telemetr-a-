45811d58c848e95398b9f366efdf882121ea2350
edRan21 <165865586+edRan21@users.noreply.github.com>
2025-04-26 22:24:57 -0600

    Delete Core directory

--- Core/Hardware/ModbusUtils.py
+++
@@ -1,8 +0,0 @@
-import serial.tools.list_ports
-
-def obtener_puertos_com():
-    """
-    Retorna una lista de dispositivos COM disponibles usando pyserial.
-    """
-    ports = serial.tools.list_ports.comports()
-    return [port.device for port in ports]
